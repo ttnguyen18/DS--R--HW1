b5a8d82304079f1d7277ad9584871204cb468d1b
fkhoshnasib <31481856+fkhoshnasib@users.noreply.github.com>
2020-01-14 01:08:06 -0500

    Add files via upload

---
+++ Fariba Khoshnasib.r
@@ -0,0 +1,45 @@
+#Fariba Khoshnasib
+
+
+2017^(1/3)
+
+#12.63481
+
+
+abs(5.7-6.8)/.58
+
+#1.896552
+
+
+a = 1:12
+a
+
+# 1  2  3  4  5  6  7  8  9 10 11 12
+
+
+b = c(1, 3, 5, 7, 9, 11)
+b
+# 1  3  5  7  9 11
+
+
+c = seq(1,11, 2)
+c
+# 1  3  5  7  9 11
+
+
+ln.a = log(a)
+ln.a
+#[1] 0.0000000 0.6931472 1.0986123 1.3862944 1.6094379 1.7917595 1.9459101
+#[8] 2.0794415 2.1972246 2.3025851 2.3978953 2.4849066
+
+c^2
+# 1   9  25  49  81 121
+
+?sd
+#This function computes the standard deviation of the values in x. If na.rm is TRUE then missing values are removed before computation proceeds.
+#sd(x, na.rm = FALSE)
+
+Name = "Fariba"
+paste("My name is", Name)
+#[1] "My name is Fariba"
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Elements-of-data-science
+# Assignment 1, Data Science 710
+
+This week's assignment has 2 parts, to be submitted using GitHub:
+
+* Run the code in assignment1_python.ipynb, make the adjustments described, and upload the resulting notebook.
+* Submit a .R file with your R code and its output, as described in assignment1_r.docx.
