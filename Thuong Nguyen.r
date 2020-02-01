7050dca1ceed16f059f0e8a52dcf90dfcac7b027
ttnguyen18 <56488291+ttnguyen18@users.noreply.github.com>
2020-02-01 13:51:40 -0500

    Add files via upload

---
+++ Thuong Nguyen.r
@@ -0,0 +1,16 @@
+2017^(1/3) #12.63481
+abs(5.7-6.8)/.58 #1.896552
+a = 1:12
+a #  1  2  3  4  5  6  7  8  9 10 11 12
+b = c(1, 3, 5, 7, 9, 11)
+b #  1  3  5  7  9 11
+c = seq(1,11, 2)
+c #  1  3  5  7  9 11
+ln.a = log(a)
+ln.a # 0.0000000 0.6931472 1.0986123 1.3862944 1.6094379 1.7917595 1.9459101 2.0794415 2.1972246 2.3025851 2.3978953 2.4849066
+sqrt(b) #1.000000 1.732051 2.236068 2.645751 3.000000 3.316625
+?sd
+#This function computes the standard deviation of the values in x. If na.rm is TRUE then missing values are removed before computation proceeds.
+Name="Thuong"
+paste("My name is", Name) #"My name is Thuong"
+
